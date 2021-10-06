b567d0d87f7b07c64d4d4f984d36b689d6aa864b
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-10-06 19:21:18 +0200

    creating class Engine, in main class creating two objects and complementing them with values

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,5 +9,17 @@ public class App {
         Car myCar2 = new Car("myCar2");
         myCar2.printName();
 
+        Engine engine1 = new Engine();
+        engine1.power = 150;
+        engine1.capacity = 1500;
+
+        Engine engine2 = new Engine();
+        engine2.power = 250;
+        engine2.capacity = 2500;
+
+        System.out.println(engine1.power);
+        System.out.println(engine1.capacity);
+        System.out.println(engine2.power);
+        System.out.println(engine2.capacity);
     }
 }
---
+++ src/main/java/app/Engine.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Engine {
+
+    public Integer power;
+    public Integer capacity;
+}
