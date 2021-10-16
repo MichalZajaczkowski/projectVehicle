9e6e2d135b61f4681894ce1a614f49ec220f290a
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-10-16 19:40:42 +0200

    if else statements with Calculate class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,13 +3,17 @@ package app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("___Calculator_____");
-
         Calculator calculator = new Calculator();
-        System.out.println(calculator.divide(6, 3));
-        System.out.println(calculator.divide(5, 45));
-        System.out.println(calculator.divide(1, 23));
-        System.out.println(calculator.divide(13, 3));
+        Double result = calculator.divide(12, 5);
 
+        if (isPositive(result)) {
+            System.out.println(result + " > 0");
+        } else if (!isPositive(result)) {
+            System.out.println(result + " <= 0 ");
+        }
+    }
+
+    private static boolean isPositive(Double result) {
+        return result > 0;
     }
 }
