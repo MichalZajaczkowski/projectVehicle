b5d465bd68022a897e85997b1a0e046584769a32
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2021-10-17 19:58:33 +0200

    loops

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,52 +6,33 @@ import app.vehicle.Car;
 public class App {
     public static void main(String[] args) {
 
-        Integer number = 12;
+        Integer i = -5;
+        Integer z = 5;
+        Integer x = 5;
 
-        switch (number % 2) {
-            case 0:
-                System.out.println("liczba parzysta switch");
-                break;
-            case 1:
-                System.out.println("liczba nieparzysta switch");
-                break;
+
+        System.out.println("----While-----"); //nie znamy liczby obiegów w pętli
+
+        while (i < 0 ) {
+            System.out.println(i);
+            i++;
+        }
+        while (z > 0 ) {
+            System.out.println(z);
+            z--;
         }
 
-        if (number % 2 == 0) {
-            System.out.println("liczba parzysta if");
-        } else {
-            System.out.println("liczba nieparzysta if");
+        System.out.println("----doWhile-----");
+        do{
+            System.out.println(x);
+            x--;
+        } while(x > 0);
+
+        System.out.println("----for-----");// znamy liczbę obiegów w pętli
+        for (int c = 0; c < 5; c++) {
+            System.out.println(c);
         }
 
-        Car car1 = new Car("car1", 200);
-        Car car2 = new Car("car2", 200);
 
-        String nameOfFasterCar = getNameOfFasterCar(car1, car2);
-        System.out.println(nameOfFasterCar);
-
-        printNameOfFasterCar(car1, car2);
     }
-
-    private static String getNameOfFasterCar(Car car1, Car car2) {
-        String result;
-        if (car1.getMaxSpeed() > car2.getMaxSpeed()) {
-            result = car1.getName();
-        } else if (car1.getMaxSpeed() < car2.getMaxSpeed()) {
-            result = car2.getName();
-        } else {
-            result = ("Prędkości są równe");
-        }
-        return result;
-    }
-
-    private static void printNameOfFasterCar(Car car1, Car car2) {
-        if (car1.getMaxSpeed() > car2.getMaxSpeed()) {
-            car1.printName();
-        } else if (car1.getMaxSpeed() < car2.getMaxSpeed()) {
-            car2.printName();
-        } else {
-            System.out.println("Prędkości są równe");
-        }
-    }
-
 }
